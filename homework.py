3ab4f06a06a9c9721cd744b9d9d2d880170b614e
AsyaYurysheva <anaiakimova@yandex.ru>
2021-04-18 11:06:23 +0800

    With review changes

--- homework.py
+++ homework.py
@@ -11,25 +11,41 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRAKTIKUM_API = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+STATUSES = {
+    'rejected': 'К сожалению в работе нашлись ошибки.',
+    'approved':
+    'Ревьюеру всё понравилось, можно приступать к следующему уроку.',
+}
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
+    filename='main.log', filemode='w')
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = ('Ревьюеру всё понравилось,'
-                   ' можно приступать к следующему уроку.')
+    homework_name = homework.get('homework_name')
+    hw_status = homework.get('status')
+    if hw_status not in STATUSES:
+        logging.info(hw_status, exc_info=True)
+        return f'Неизвестный статус работы "{homework_name}".'
+    verdict = STATUSES[hw_status]
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    homework_statuses = requests.get(
-        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        params={'from_date': current_timestamp},
-        headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
-    )
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(
+            PRAKTIKUM_API,
+            params={'from_date': current_timestamp},
+            headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
+        )
+        return homework_statuses.json()
+
+    except requests.exceptions.RequestException as e:
+        logging.error(e, exc_info=True)
+        raise SystemExit(e)
 
 
 def send_message(message, bot_client):
@@ -38,12 +54,15 @@ def send_message(message, bot_client):
 
 def main():
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
-        filename='main.log', filemode='w')
     logging.debug('Бот запущен.')
     current_timestamp = int(time.time())
+    new_homework = []
+
+    def catch_index_error(index):
+        try:
+            return new_homework.get('homeworks')[index]
+        except IndexError:
+            return None
 
     while True:
         try:
@@ -51,7 +70,7 @@ def main():
             if new_homework.get('homeworks'):
                 send_message(
                     parse_homework_status(
-                        new_homework.get('homeworks')[0]),
+                        catch_index_error(0)),
                     bot_client
                 )
             current_timestamp = new_homework.get(
